7ada28cb1746b2961c9d09cd3097f679c8819d94
KSohal <kavannn@live.com>
2020-12-03 10:11:10 +0000

    Added GP class customisation ToString method

---
+++ PRMS/Model/Customised Classes/GpToString.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model
+{
+	public partial class Gp
+	{
+		public override string ToString()
+		{
+			return $"{FirstName} {LastName}";
+		}
+	}
+}
